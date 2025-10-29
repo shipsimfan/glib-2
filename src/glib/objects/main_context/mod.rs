0c38e7c3437b9e0e25ef9ab420c9f40e32cdb94f
Lance Hart <lhart@aagcnc.com>
2025-10-29 08:58:24 -0400

    Added new and unref for GMainContext

--- src/glib/objects/main_context/mod.rs
+++ src/glib/objects/main_context/mod.rs
@@ -2,17 +2,21 @@ use std::ffi::c_void;
 
 mod default;
 mod get_thread_default;
+mod new;
 mod pop_thread_default;
 mod push_thread_default;
 mod r#ref;
 mod ref_thread_default;
+mod unref;
 
 pub use default::g_main_context_default;
 pub use get_thread_default::g_main_context_get_thread_default;
+pub use new::g_main_context_new;
 pub use pop_thread_default::g_main_context_pop_thread_default;
 pub use push_thread_default::g_main_context_push_thread_default;
 pub use r#ref::g_main_context_ref;
 pub use ref_thread_default::g_main_context_ref_thread_default;
+pub use unref::g_main_context_unref;
 
 /// The [`GMainContext`] struct is an opaque data type representing a set of sources to be handled
 /// in a main loop.
