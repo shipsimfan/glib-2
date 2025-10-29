cd0e2495bd99d907c122409058d4863f5ac2bbaf
Lance Hart <lhart@aagcnc.com>
2025-10-29 11:04:23 -0400

    Added g_object_set

--- src/gobject/functions/mod.rs
+++ src/gobject/functions/mod.rs
@@ -2,6 +2,7 @@ mod g_clear_object;
 mod g_object_get;
 mod g_object_get_data;
 mod g_object_ref;
+mod g_object_set;
 mod g_object_set_data;
 mod g_object_unref;
 
@@ -9,5 +10,6 @@ pub use g_clear_object::g_clear_object;
 pub use g_object_get::g_object_get;
 pub use g_object_get_data::g_object_get_data;
 pub use g_object_ref::g_object_ref;
+pub use g_object_set::g_object_set;
 pub use g_object_set_data::g_object_set_data;
 pub use g_object_unref::g_object_unref;
